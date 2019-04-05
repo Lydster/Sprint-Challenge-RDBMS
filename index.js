00b941d3c775a0ab815e9be9dcd1b126e04fb0a0
lydiathornton <lydia.d.thornton@gmail.com>
2019-04-05 10:00:19 -0700

    get request working on server - next write knex query to join actions

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const helmet = require("helmet");
 const db = require("./data/dbConfig");
-
-//import helpers later!! don't forget >:o
+const { getProjects } = require("./helpers/project-helpers");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
+server.get("/api/projects", (req, res) => {
+  getProjects()
+    .then(projects => {
+      res.status(200).json(projects);
+      console.log(projects);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "cound not get projects." });
+    });
+});
+
 const port = 5000;
 server.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}!!`);
 });
--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,7 @@ module.exports = {
     connection: {
       filename: "./projects.sqlite3"
     },
+    useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
     },
